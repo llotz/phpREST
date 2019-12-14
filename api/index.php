3e17a06212ccfa63dbbbc4c28d0a659d8e51df35
Lukas Lotz <lukas@lotz24.de>
2019-12-14 23:55:10 +0100

    Added Endpoint location

--- api/index.php
+++ api/index.php
@@ -1,7 +1,26 @@
-<?php
-
-?> 
-
 <h2>phpREST</h2>
 <p>This is a API library implemented in php.</p>
-<p>Automatic API description will come soon.</p>
+<p>API description will bee added soon.</p>
+
+<h2>Endpoints</h2>
+
+<?php
+$baseLink = getBaseLink();
+$apis = scandir("api");
+if(count($apis) > 2){
+	for($i = 2; $i < count($apis); $i++){
+		$apiName = $apis[$i];
+		echo "<p><a href='$baseLink/api/$apiName'>$baseLink/api/$apiName/</a></p>";
+	}
+}
+
+function getBaseLink(){
+	$http = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http");
+	$host = $_SERVER['HTTP_HOST'];
+	$baseLink = "$http://$host";
+	return $baseLink;
+}
+
+
+
+?> 
